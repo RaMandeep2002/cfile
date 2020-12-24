cdeb30b2770d9670dec9bc63eec757cd81d58108
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2020-12-24 15:37:30 +0530

    Create 03_partices.c

---
+++ cfile/03_partices.c
@@ -0,0 +1,11 @@
+// how to get averge of three number
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, avg;
+    printf("Enter the three number: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    return 0;
+}
