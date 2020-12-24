5de4e9e0de9e29b182fa3c4bbab46d99de5027d3
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2020-12-24 11:13:23 +0530

    Create 02_paratics.c
    two add a two number

---
+++ cfile/02_paratics.c
@@ -0,0 +1,13 @@
+// add of two number
+#include <stdio.h>
+
+int main()
+{
+    int a, b, total;
+    printf("Enter the two number:");
+    scanf("%d %d", &a, &b);
+
+    total = a + b;
+    printf("here is the total: %d", total);
+    return 0;
+}
