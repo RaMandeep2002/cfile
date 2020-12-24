02902f550aaba2301d9dd2e285f216e48ff9d37b
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2020-12-24 15:40:13 +0530

    to get a averge of three  number

--- cfile/03_partices.c
+++ cfile/03_partices.c
@@ -1,11 +1,19 @@
-// how to get averge of three number
+// Here is the header file
+// stdio is stand for standand input and output header.
 #include <stdio.h>
 
 int main()
 {
-    int a, b, c, avg;
-    printf("Enter the three number: \n");
-    scanf("%d %d %d", &a, &b, &c);
-    
+    //here i declare the variable num1, num2, num3.
+    int num1, num2, num3;
+    //here i declare the variable float as avg
+    //because the averge is the point value
+    float avg;
+    printf("Enter the three number which you have get the avg:- ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    //here is the calculation of the averge.
+    avg = (num1 + num2 + num3) / 3;
+    //here is the output
+    printf("\nAverage: %f", avg);
     return 0;
 }
