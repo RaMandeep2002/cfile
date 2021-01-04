af7fe0e3bc43dbe5058078ebd4e3393bac844ea0
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2021-01-04 18:56:56 +0530

    simple add of two number

--- cfile/03_add_of_two_number.c
+++ cfile/03_add_of_two_number.c
@@ -2,6 +2,11 @@
 
 int main()
 {
+    int i, j, add;
+    printf("Enter the two number: - ");
+    scanf("%d %d", &i, &j);
 
+    add = i + j;
+    printf("Addition of the two number is  %d", add);
     return 0;
 }
