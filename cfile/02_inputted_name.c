a31a5cce3e2b73ac7747d85b9cc19ec5ada056f2
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2021-01-04 18:02:44 +0530

    program of inputted name

---
+++ cfile/02_inputted_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char name[30];
+    printf("Enter the name: -");
+    scanf("%s", &name);
+    printf("hello, my name is %s", name);
+    return 0;
+}
