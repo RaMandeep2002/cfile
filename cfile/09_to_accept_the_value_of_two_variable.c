32506f79931cf2f305043ae753ff3a607103372b
RaMandeep2002 <76144050+RaMandeep2002@users.noreply.github.com>
2021-01-07 22:48:03 +0530

    Update 09_to_accept_the_value_of_two_variable.c

--- cfile/09_to_accept_the_value_of_two_variable.c
+++ cfile/09_to_accept_the_value_of_two_variable.c
@@ -1,15 +1,42 @@
+// /*
+// In this code we have swaping of two number.
+// */
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 10, b = 20, temp;
+//     printf("Before swaping the number a = %d, b = %d\n", a, b);
+//     temp = a;
+//     a = b;
+//     b = temp;
+//     printf("After swaping the number a = %d, b = %d", a, b);
+//     return 0;
+// }
+
 /*
-In this code we have swaping of two number.
+In the code we have swaping of two number when user enter a number
 */
 #include <stdio.h>
 
 int main()
 {
-    int a = 10, b = 20, temp;
-    printf("Before swaping the number a = %d, b = %d\n", a, b);
-    temp = a;
-    a = b;
-    b = temp;
-    printf("After swaping the number a = %d, b = %d", a, b);
+    int first, second, temp;
+    printf("Enter the first number: \n");
+    scanf("%d", &first);
+    printf("Enter the second number: \n");
+    scanf("%d", &second);
+
+    // value of the first is assigned to temp
+    temp = first;
+
+    //value of second is assigned to first
+    first = second;
+
+    //value of temp (inital value of first) is assigned to second
+    second = temp;
+
+    printf("\nAfter swaping , first number = %d\n", first);
+    printf("After swaping , second number = %d", second);
     return 0;
 }
